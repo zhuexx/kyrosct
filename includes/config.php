1e1f641ad6e4ed4c1481818e1c6be2fec6141c55
zhuexx <zhuexxx@gmail.com>
2025-07-12 10:13:41 +0800

    Create config.php

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+// TinyHost MySQL info (get from your dashboard)
+define('DB_HOST', 'mysql.tinyhost.me');
+define('DB_USER', 'tiny_XXXXX'); 
+define('DB_PASS', 'your_password');
+define('DB_NAME', 'tiny_XXXXX_keys');
+
+// Secret token (change this!)
+define('ADMIN_TOKEN', 'TINYHOST-SECRET-123');
+?>
